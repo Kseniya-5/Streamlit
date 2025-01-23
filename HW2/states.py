a717ac3ca8e87fe65c0b264f6b4a8bfdc576d570
Kseniia <ksbalitskaya@edu.hse.ru>
2025-01-24 01:21:52 +0300

    add states

--- HW2/states.py
+++ HW2/states.py
@@ -1,6 +1,6 @@
 from aiogram.fsm.state import State, StatesGroup
 
-class Profile(StatesGroup):
+class User(StatesGroup):
     name = State()  # Состояние для имени
     weight = State()  # Состояние для веса
     height = State()  # Состояние для роста
@@ -8,3 +8,6 @@ class Profile(StatesGroup):
     activity_level = State()  # Состояние для уровня активности
     city = State()  # Состояние для города
     calorie_goal = State()  # Состояние для цели калорий
+    logged_water = State() # Состояние для выпитой воды
+    logged_calories = State() # Состояние для полученных калорий
+    burned_calories = State() # Состояние для сожженых калорий
