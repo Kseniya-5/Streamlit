5addf1da7a7040d2301669ff46ebcf77ffb3e2a0
Kseniia <ksbalitskaya@edu.hse.ru>
2025-01-24 01:21:09 +0300

    add API_WEATHER

--- HW2/config.py
+++ HW2/config.py
@@ -4,5 +4,8 @@ from  dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('BOT_TOKEN')
+API_W = os.getenv('API_WEATHER')
 if not TOKEN:
     raise ValueError('Переменная окружения BOT_TOKEN не установлена!')
+elif not API_W:
+    raise ValueError('Переменная окружения API_WEATHER не установлена!')
